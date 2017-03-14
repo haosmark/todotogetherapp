ee9bbb60b7bc3b9f7748bb1c51c272abc991ce2f
Denis St <haosmark@gmail.com>
2017-03-14 12:44:34 -0400

    Implement TaskItem POCO

---
+++ ToDoTogetherApp/Models/TaskItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoTogetherApp.Models
+{
+    public class TaskItem
+    {
+        public string Id { get; set; }
+        public string ProjectId { get; set; }
+        public string Name { get; set; }
+        public bool Complete { get; set; }
+    }
+}
