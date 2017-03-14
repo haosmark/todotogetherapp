0b1be2f98e32e60fda878191ef3ecc8ae48e725a
Denis St <haosmark@gmail.com>
2017-03-14 12:17:40 -0400

    Add project files.

---
+++ ToDoTogetherApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Security.Credentials;
+
+namespace ToDoTogetherApp.ViewModels
+{
+    public class LoginViewModel
+    {
+        private MobileServiceUser user;
+        private PasswordVault vault = new PasswordVault();
+        private PasswordCredential credential;
+
+        public bool IsAuthenticated()
+        {
+            var provider = MobileServiceAuthenticationProvider.Facebook;
+            try
+            {
+                credential = vault.FindAllByResource(provider.ToString()).FirstOrDefault();
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            if (credential != null)
+            {
+                user = new MobileServiceUser(credential.UserName);
+                credential.RetrievePassword();
+                user.MobileServiceAuthenticationToken = credential.Password;
+
+                App.MobileService.CurrentUser = user;
+                return true;
+            }
+
+            return false;
+        }
+
+        public async Task<bool> AuthenticateAsync()
+        {
+            var provider = MobileServiceAuthenticationProvider.Facebook;
+
+            try
+            {
+                user = await App.MobileService.LoginAsync(provider);
+                credential = new PasswordCredential(provider.ToString(), user.UserId, user.MobileServiceAuthenticationToken);
+                vault.Add(credential);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ToDoTogetherApp/Views/LoginView.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using ToDoTogetherApp.ViewModels;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ToDoTogetherApp.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class LoginView : Page
+    {
+        LoginViewModel vm;
+
+        public LoginView()
+        {
+            this.InitializeComponent();
+            vm = new LoginViewModel();
+        }
+
+        private async void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            await vm.AuthenticateAsync();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (vm.IsAuthenticated())
+            {
+                // todo: navigate away
+            }
+        }
+    }
+}
