c4efd23d941af555b17f1d02c74e9e4d6ece80f2
Denis St <haosmark@gmail.com>
2017-03-19 12:55:34 -0400

    Add functionality code to get contributor projects

---
+++ ToDoTogetherApp/ViewModels/OtherProjectsViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoTogetherApp.Common;
+using ToDoTogetherApp.Models;
+
+namespace ToDoTogetherApp.ViewModels
+{
+    public class OtherProjectsViewModel
+    {
+        ClientAPI api = new ClientAPI(App.MobileService.MobileAppUri.ToString());
+
+        public ObservableCollection<Project> Projects { get; set; }
+        private User currentUser { get; set; }
+
+        public OtherProjectsViewModel()
+        {
+            Projects = new ObservableCollection<Project>();
+        }
+
+        public async Task GetProjectsAsync()
+        {
+            if (currentUser == null)
+            {
+                currentUser = await App.MobileService.InvokeApiAsync<User>("Facebook", HttpMethod.Get, null);
+            }
+            var myProjects = await api.GetProjectsByContributorAsync(currentUser.Id);
+
+            foreach (var p in myProjects)
+            {
+                Projects.Add(p);
+            }
+        }
+    }
+}
